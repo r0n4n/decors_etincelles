beea7b02df504d6d31907577043279ff4d62cf69
r0n4n <ronan.blanchard35@gmail.com>
2023-04-12 00:06:11 +0200

    création du module WirelessShow

--- Emetteur_DMX/Emetteur_DMX.ino
+++ Emetteur_DMX/Emetteur_DMX.ino
@@ -1,6 +1,6 @@
 // Programme permettant la lecture de la tram DMX et envoit des donnÃƒÂ©es par liaison RF a tous les dÃƒÂ©cors
 
-//#define DMX
+#define DMX
 
 #ifdef DMX
 #include <DMXSerial.h>                   // Appel de la librairie SerialDMX
@@ -9,33 +9,17 @@ int mode = 1 ; // 1==DMX transmitter ; 2 == Receiver
 int mode = 4 ; // 1==DMX transmitter ; 2 == Receiver ; 3 == direct transmitter ; 4 : Manual DMX Transmitter
 #endif
 
-#include <RFM69.h> //get it here: https://www.github.com/lowpowerlab/rfm69
+//#include <RFM69.h> //get it here: https://www.github.com/lowpowerlab/rfm69
 
-#include "parameters.h"
+
 #include "DmxEffects.h"
+#include "WirelessShow.h"
+
+#define NO_DATA_SINCE 3000
+#define SERIAL_BAUD 115200
 
 #define DEBUG
 
-
-//*********************************************************************************************
-// *********** IMPORTANT SETTINGS - YOU MUST CHANGE/ONFIGURE TO FIT YOUR HARDWARE *************
-//*********************************************************************************************
-#define NETWORKID 100 // The same on all nodes that talk to each other
-#define NODEID 1 // The unique identifier of this node
-
-
-//******************* LIST OF RECEIVERS *****************
-#define NODERECEIVE 2
-//*******************************************************
-
-//Match frequency to the hardware version of the radio on your Feather
-#define FREQUENCY RF69_433MHZ
-#define IS_RFM69HCW true // set to 'true' if you are using an RFM69HCW module
-
-#define RFM69_CS 10 // Chip select
-#define RFM69_IRQ 2 // interruption 
-#define RFM69_IRQN 0 // Pin 2 is IRQ 0! 
-#define RFM69_RST 9 // Reset 
 
 //*********************   DEFINE OUT/IN PINS **************************
 #define LED 8 // onboard blinky
@@ -49,14 +33,7 @@ uint8_t  *dmxData;
 uint8_t  manData[DMXSERIAL_MAX];
   
 
-/****************** DEFINE LIST ***************************/
-#define PACKET_SIZE 60 // number of channels to send per packet
-#define PACKET_SIZE_PLUS_ID  PACKET_SIZE+1 // total size of a packet with the ID  
-#define PACKET_NBR 8 // nombre paquet que l'on souhaite envoyer 
-#define PACKET_AVAILABLE 8 // nombre de paquets qui dÃƒÂ©composent l'ensemble des canaux DMX 
-#define NO_DATA_SINCE 3000
-#define SERIAL_BAUD 115200
-/*************************************************************/
+
 
 
 uint8_t last_dmx_channels[513] ;
@@ -68,14 +45,8 @@ unsigned long iterations = 0;
 unsigned long decimation = 100000;
 long lastPeriod = -1;
 
-typedef struct {
-  uint8_t           packetId; //store this nodeId
-  uint8_t packet[PACKET_SIZE];
-} Payload;
-Payload theData;
 
 
-RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
 
 void setup () { // Configuration au dÃƒÂ©marrage
   IOinit();
@@ -102,17 +73,17 @@ void loop () { // Boucle du programme principal
   switch (mode){
     case 1: 
     #ifdef DMX
-      checkDMXCom(); 
+      //checkDMXCom(); 
     #endif
       break;
     case 2:
     #ifndef DMX
-      checkRFMReception();
+//      checkRFMReception();
     #endif
       break;
     case 3:
     #ifndef DMX
-      sendFromMonitor();
+      //sendFromMonitor();
     #endif
       break;
     case 4:
@@ -175,44 +146,12 @@ void sendManualTram(){
 
 
 
-void sendPackets(uint8_t  *trame){
-  uint8_t radiopacket[PACKET_SIZE_PLUS_ID] ;
-  
-  for (int i = 1 ; i <=PACKET_NBR ; i++){
-      build_packet(i, trame, radiopacket); // construction et envoi du paquet i   
-      radio.send(NODERECEIVE, (const void*)radiopacket, strlen(radiopacket), false) ; // envoi du paquet de donnÃƒÂ©es
-    }
-}
 
-/*
- * Construit un paquet de plusieurs canaux DMX selon le packet_id donnÃƒÂ©es en paramÃƒÂ¨tre. 
- * @param int packet_id : l'identifiant du paquet ÃƒÂ  envoyer  
- * @return None. 
- */
-void build_packet(int packet_id, uint8_t  *trame , uint8_t  *packet ) {
-  packet[0] = packet_id ; // L'identifiant du paquet est le premier byte du paquet ÃƒÂ  envoyer
-  int channel_offset = (packet_id - 1) * PACKET_SIZE ; // calcul de l'offset du canal DMX selon le packet_id
-
-  for (int i = 1 ; i < (PACKET_SIZE + 1) ; i++) { // construction du paquet ÃƒÂ  envoyer avec les canaux DMX
-    packet[i] = trame[i + channel_offset] ;
-    delete_zeros(packet, i) ; 
-  }
-}
-
-/*
- * delete_zeros regarde si la valeur du canal donnÃƒÂ©e en paramÃƒÂ¨tre est nulle. Si oui elle la chnage en 1. Cette function est utilisÃƒÂ©e pour permettre l'envoi des donnÃƒÂ©es via    
- * le module RFM69. Le module ne prends pas les valeurs nulles.
- * @param int i : le canal concernÃƒÂ© 
- * @return None. 
- */
-void delete_zeros(uint8_t *packet, int i){
-   if (packet[i] == 0 )
-      packet[i] = 1 ; // rejet des zeros sur les canaux DMX pour ÃƒÂ©viter les erreurs de transmission sur la liason RFM69
-}
 
 
 /********* Envoi les paquets l'un aprÃƒÂ¨s l'autre *******/
 #ifdef DMX
+
 void _DMX_RFM69_send(void) {
    // digitalWrite(LED,HIGH) ; 
    if (dmx_change()) {
@@ -267,23 +206,23 @@ void debug_channels_change(void) {
 #endif
 
 #ifndef DMX
-void sendFromMonitor(void){
+/*void sendFromMonitor(void){
     //fill in the struct with new values
     theData.packetId = 3;
     theData.packet[0] = 255;
     
     Serial.print("Sending struct (");
     Serial.print(sizeof(theData));
-    Serial.print(" bytes) ... ");
+    Serial.print(" bytes) ... ");*/
     /*if (radio.sendWithRetry(NODERECEIVE, (const void*)(&theData), sizeof(theData)))
       Serial.print(" ok!");
     else Serial.print(" nothing...");*/
-    
+    /*
     radio.send(NODERECEIVE, (const void*)(&theData), sizeof(theData));
     Serial.println();
 
  
-}
+}*/
 #endif
 
 
@@ -293,25 +232,11 @@ void IOinit(void){
   pinMode(DEBUGPIN, OUTPUT) ;
 }
 
-void wireless_init(void){
-  // Hard Reset the RFM module
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, HIGH);
-  delay(100);
-  digitalWrite(RFM69_RST, LOW);
-  delay(100);
 
-  // Initialize radio
-  radio.initialize(FREQUENCY, NODEID, NETWORKID);
-  if (IS_RFM69HCW) {
-    radio.setHighPower(); // Only for RFM69HCW & HW!
-  }
-  radio.setPowerLevel(31); // power output ranges from 0 (5dBm) to 31 (20dBm)
-  radio.encrypt(ENCRYPTKEY);
-}
 
 
 #ifndef DMX
+/*
 void checkRFMReception(){
   if (radio.receiveDone())
   {     
@@ -331,7 +256,7 @@ void checkRFMReception(){
     //Serial.println(iterations);
     iterations= iterations + 1;
   }  
-}
+}*/
 #endif
 
 void _noDataSince() {
