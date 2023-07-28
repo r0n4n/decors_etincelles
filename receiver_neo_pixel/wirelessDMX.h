9b6fcb7c71b693d1781840182c6fe921e64d4a95
r0n4n <ronan.blanchard35@gmail.com>
2023-07-28 14:50:11 +0200

    gestion des adresses réseau par costume

--- receiver_neo_pixel/wirelessDMX.h
+++ receiver_neo_pixel/wirelessDMX.h
@@ -3,25 +3,35 @@
 
 #define NO_DATA_SINCE 3000
 
+// ***************** Configuration réseau *********************
+#define NETWORKID 100 //the same on all nodes that talk to each other
+#define TRANSMITTERID 1 // L'adresse réseau de l'émetteur
+#define BROADCASTID 2 // L'adresse où le DMX est broadcasté 
+#define NODEID 13 // L'adresse réseau du récepteur 
+#define TESTEURID 4 // L'adresse réseau du testeur réseau 
+//***********************************************************************
+
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      50.0
 #define CHANNELS_PER_PIXEL 3 // RVB
 #define PIX_PER_GROUP 1 // number of pixels together 
 #define PACKET_SIZE 60 // size of a packet received
-#define DECOR_DMX_ADRESS  1 // adresse DMX du récepteur 
+
+#if NODEID==11 || NODEID==14 || NODEID==17
+  #define DECOR_DMX_ADRESS  1 // adresse DMX du récepteur 
+#elif NODEID==12 || NODEID==15 || NODEID==18 
+  #define DECOR_DMX_ADRESS  151 // adresse DMX du récepteur 
+#elif NODEID==13 || NODEID==16 || NODEID==19 
+  #define DECOR_DMX_ADRESS  301 // adresse DMX du récepteur 
+#endif
+
 #define PACKET_ID_MAX 9 // nombre de paquets maximal que peut envoyer l'émetteur (dépend de la taille des paquets)  
 #define PACKET_NBR (int)8 // nombre de paquets envoyés par l'emetteur 
 
 #define CHANNELS_NBR (NUMPIXELS*CHANNELS_PER_PIXEL/PIX_PER_GROUP) // on détermine le nombre de canaux nécessaires
 #define LAST_DMX_ADRESS (DECOR_DMX_ADRESS+CHANNELS_NBR-1)
 
-// ***************** Configuration réseau *********************
-#define NETWORKID 100 //the same on all nodes that talk to each other
-#define TRANSMITTERID 1 // L'adresse réseau de l'émetteur
-#define BROADCASTID 2 // L'adresse où le DMX est broadcasté 
-#define NODEID 11 // L'adresse réseau du récepteur 
-#define TESTEURID 4 // L'adresse réseau du testeur réseau 
-//***********************************************************************
+
 /** CODE DIAG **/ 
 #define AUTO 0 
 #define REMOTEMANUAL 1  
